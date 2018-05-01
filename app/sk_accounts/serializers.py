b3d9d107e4bb85240a254ff7f10f416cbd7c5b3f
Spencer Cooley <contact@spencercooley.com>
2018-04-30 18:54:38 -0700

    working on the wrong branch so merged correct changes in .

--- app/sk_accounts/serializers.py
+++ app/sk_accounts/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    #unique email and unique username
+
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=User.objects.all(), message="There is already an account associated with that email address.")])
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            is_active=False,
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
     class Meta:
         model = User
-        fields = ('first_name','last_name','username')
+        write_only_fields = 'password'
+        fields = ('first_name','last_name','username', 'email', 'password')
--- app/sk_accounts/views.py
+++ app/sk_accounts/views.py
@@ -5,11 +5,35 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
 import json
+from rest_framework.permissions import AllowAny, IsAdminUser
 
-class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+class MixedPermissionModelViewSet(viewsets.ModelViewSet):
+   '''
+   Mixed permission base model allowing for action level
+   permission control. Subclasses may define their permissions
+   by creating a 'permission_classes_by_action' variable.
+
+   Example:
+   permission_classes_by_action = {'list': [AllowAny],
+                                   'create': [IsAdminUser]}
+   '''
+
+   permission_classes_by_action = {'create': [AllowAny],
+                                   'list': [IsAdminUser]}
+   def get_permissions(self):
+      try:
+        # return permission_classes depending on `action`
+        return [permission() for permission in self.permission_classes_by_action[self.action]]
+      except KeyError:
+        # action is not set return default permission_classes
+        return [permission() for permission in self.permission_classes]
+
+
+class UserViewSet(MixedPermissionModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
 
 class UserInfo(APIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
