6bcb824a84a6947df7ccd10e2a2326e3276afc3f
Spencer Cooley <contact@spencercooley.com>
2018-07-22 10:12:51 -0700

    began setting up gcp kubernetes configuration.

--- app/app/settings.py
+++ app/app/settings.py
@@ -107,15 +107,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_URL = '/static/'
-AWS_STORAGE_BUCKET_NAME = os.environ["DJANGO_S3_STORAGE"]
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-
 
 
 DATABASES = {
@@ -186,3 +177,11 @@ CELERY_BEAT_SCHEDULE = {
         },
     },
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_URL = 'https://storage.googleapis.com/stage-api/'
+
+# STATIC_URL = 'https://storage.googleapis.com/<your-gcs-bucket>/static/'
+# [END staticurl]
+STATIC_ROOT = 'static/'
